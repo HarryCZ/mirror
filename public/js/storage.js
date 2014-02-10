c787cbf8a621b1064c04331d53e8154bbbce789a
HarryCZ <jakub.voboril@gmail.com>
2014-02-10 01:18:05 +0000

    provazani storage a storageItems

--- public/js/storage.js
+++ public/js/storage.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
-	
+	items = items.replace(/&quot;/g,'"');
+	itemsArr = eval("("+items+")");
+	storage = storage.replace(/&quot;/g,'"');
+	storageArr = eval("("+storage+")");
+	var i=0;
+	var matched = 0;
+	while ((itemsArr[i])&&(matched==0)) {
+		if (storageArr[0].storage_item == itemsArr[i]._id) {
+			jQuery('.usertable .title').text(itemsArr[i].title);
+			matched = 1;
+		}
+		i++;
+	}
 })
